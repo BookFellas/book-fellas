063e304766dd25a7f96d49cc8155d4f5967dfa23
Yang Chen <chenyanghmilu@gmail.com>
2020-01-27 15:10:47 -0500

    add profiles template

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('books/', views.books_index, name='index'),
     path('books/<int:book_id>/', views.books_detail, name='detail'),
     path('api/', views.api, name='api'),
+    path('profiles/', views.profiles_index, name='profiles_index'),
+    # path('profiles/<int:profile_id>/', views.profiles_detail, name='detail'),
+    # path('profiles/create/', views.profileCreate.as_view(), name='profiles_create'),
+    # path('profiles/<int:pk>/update/', views.profileUpdate.as_view(), name='profiles_update'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup', views.signup, name='signup'),
 ]
