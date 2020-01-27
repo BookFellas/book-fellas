c5a4cbe1676e236ceec6f4e5e19c90516e9be7b1
Yang Chen <chenyanghmilu@gmail.com>
2020-01-27 00:54:30 -0500

    add logo books registration

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('api/', views.api, name='api')
+    path('books/', views.books_index, name='index'),
+    path('books/<int:book_id>/', views.books_detail, name='detail'),
+    path('api/', views.api, name='api'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup', views.signup, name='signup'),
 ]
