adbf2186af6e319613750ef15ced9066c91bae41
Joaquim <pgrilo92@hotmail.com>
2020-01-30 19:17:56 -0500

    update settings for heroku

--- bookfellas/settings.py
+++ bookfellas/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ my_db = os.environ['DATABASE']
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z9%@b1z9s(y(3(28c3luwf+jn0r_9rwmh9culjuz$#rq2o!qch'
+my_secret = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -126,3 +127,4 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+django_heroku.settings(locals())
